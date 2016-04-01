5c23b638297e11469bf51f46a852c5ab95feb741
Tom Veale <vealetm@vcu.edu>
2016-03-31 17:55:33 -0700

    fixed server

--- Server.go
+++ Server.go
@@ -5,10 +5,26 @@
 
 package bf
 
+
+pimport(
+	"fmt"
+	"net/http"
+	"time"
+)
+
+
 // Takes a database for putting the cards
 // Returns the error
 func StartServer(db Database, comms chan string) {
 
+	s := &http.Server{
+		Addr:		":1337",
+		Handler:	myHandler,
+		ReadTimeout:	10 * time.Second,
+		WriteTimeout:	10 * time.Second,
+		MaxHeaderBytes:	1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
 
 func AddModule(p Plugin) {
