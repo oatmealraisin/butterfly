9da4ebc7d06d2ce9a0cbfab092131e658ea601e8
Tom Veale <vealetm@vcu.edu>
2016-03-31 22:14:13 -0700

    broken sevrlet : /

--- Server.go
+++ Server.go
@@ -5,12 +5,31 @@
 
 package bf
 
-
-pimport(
+import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
+
+/*
+func Log(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+*/
+
+
+//declare a handler, adds functionality to DefaultServeMux
+http.Handle("/foo", fooHandler)
+
+http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+//log.Fatal(http.ListenAndServe(":8080", nil))
 
 
 // Takes a database for putting the cards
@@ -18,11 +37,11 @@ pimport(
 func StartServer(db Database, comms chan string) {
 
 	s := &http.Server{
-		Addr:		":1337",
-		Handler:	myHandler,
-		ReadTimeout:	10 * time.Second,
-		WriteTimeout:	10 * time.Second,
-		MaxHeaderBytes:	1 << 20,
+		Addr:           ":1337",
+		Handler:        myHandler,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
 	log.Fatal(s.ListenAndServe())
 }
